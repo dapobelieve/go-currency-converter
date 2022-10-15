93e042e00bda5b6696808c0b3891b8c87c87a2e8
kingsloc <kingsleynwankwou@gmail.com>
2022-10-15 11:34:10 +0100

    added a go.mod file and fixed some import issues

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kingsbloc/go-currency-converter
+
+go 1.19
--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"./src/kingsbloc"
+	"fmt"
+
+	"github.com/kingsbloc/go-currency-converter/src/dapobelieve"
+	"github.com/kingsbloc/go-currency-converter/src/kingsbloc"
 )
 
 func main() {
+	currency := dapobelieve.Dollar(2000)
+	fmt.Println(currency)
 	// base Currency is Naira
-	currency := kingsbloc.Naira(2000)
-	currency.Euro()
+	amount := 2000
+	mycurrency := kingsbloc.Naira(amount)
+	fmt.Printf("%d Naira = %f Euros \n", amount, mycurrency.Euro())
+	fmt.Printf("%d Naira = %f Pounds \n", amount, mycurrency.Pound())
+	fmt.Printf("%d Naira = %f US Dollars \n", amount, mycurrency.UsDollar())
 }
